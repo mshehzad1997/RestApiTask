758c7d368921651741ca8cfd7cc006372fd0c298
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-18 11:57:39 +0500

    CreateRole Action

---
+++ RestApiTask/Controllers/ManageRolesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApiTask.Models;
+using RestApiTask.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageRolesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public ManageRolesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        [Route("CreateRole")]
+        public async Task<IActionResult> CreateRole(ManageRoles model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.manageRoles.Add(model);
+                await _db.SaveChangesAsync();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ RestApiTask/Migrations/20220318064913_UserAndRoles.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiTask.Migrations
+{
+    public partial class UserAndRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "manageRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Payments = table.Column<int>(type: "int", nullable: false),
+                    ManageTenants = table.Column<int>(type: "int", nullable: false),
+                    DemoRequests = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_manageRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ManageRoleId = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_users_manageRoles_ManageRoleId",
+                        column: x => x.ManageRoleId,
+                        principalTable: "manageRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_ManageRoleId",
+                table: "users",
+                column: "ManageRoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "manageRoles");
+        }
+    }
+}
