4f90b49f797e5792f96d88bf0c5b83cebe389f55
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-18 10:33:53 +0500

    RolesModel

---
+++ RestApiTask/Models/ManageRoles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiTask.Models
+{
+    public class ManageRoles
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Payment Payments { get; set; }
+        public ManageTenant ManageTenants { get; set; }
+        public DemoRequest DemoRequests { get; set; }
+        public enum Payment
+        {
+            ViewPayment,
+            ChangeStatus
+
+        }
+        public enum ManageTenant
+        {
+            RegisterTenant,
+            ViewTenant,
+            UpdateTenant,
+            UpgradePlan,
+            RessetPassword
+        }
+        public enum DemoRequest
+        {
+            ViewRequest,
+            ApproveRequest
+        }
+    }
+}
