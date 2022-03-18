c85694b76758ec245c7150b1e85537d2dc4834ad
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-18 16:00:03 +0500

    UniqueRoleName Attribute Added

---
+++ RestApiTask/Models/UniqueRoleNameAttribute.cs
@@ -0,0 +1,29 @@
+﻿using RestApiTask.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiTask.Models
+{
+    public class UniqueRoleNameAttribute:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+            var entity = _context.manageRoles.SingleOrDefault(e => e.RoleName == value.ToString());
+
+            if (entity != null)
+            {
+                return new ValidationResult(GetErrorMessage(value.ToString()));
+            }
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage(string rolename)
+        {
+            return $"RoleName {rolename} is already in use.";
+        }
+    }
+}
