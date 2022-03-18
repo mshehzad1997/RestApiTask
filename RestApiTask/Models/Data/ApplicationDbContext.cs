13d576745cf8b180254d1227cf74ba8d8d0ed7f0
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-18 10:42:22 +0500

    ManageRole and Users Table Created

--- RestApiTask/Models/Data/ApplicationDbContext.cs
+++ RestApiTask/Models/Data/ApplicationDbContext.cs
@@ -10,7 +10,9 @@ namespace RestApiTask.Models.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
         {
-
+         
         }
+        public DbSet<ManageRoles> manageRoles { get; set; }
+        public DbSet<Users> users { get; set; }
     }
 }
