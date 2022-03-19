c8672df26432060d8425364b0be41617775df1f2
mshehzad1997 <geshehzad@gmail.com>
2022-03-19 19:41:09 +0500

    new migration added

---
+++ RestApiTask/Migrations/20220319143100_createRoleandUse.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiTask.Migrations
+{
+    public partial class createRoleandUse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
