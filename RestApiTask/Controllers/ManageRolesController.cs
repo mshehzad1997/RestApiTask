3eda25ebaad217a9cbc2b2c011f4ff8bbf69e899
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-21 15:25:38 +0500

    user delete role update

--- RestApiTask/Controllers/ManageRolesController.cs
+++ RestApiTask/Controllers/ManageRolesController.cs
@@ -166,7 +166,7 @@ namespace RestApiTask.Controllers
             }
             catch
             {
-                return BadRequest("Cannot delete Role is already in use");
+                return BadRequest();
             }
            
             return Ok();
