1c830a6534601cf3636067bba9f4c1dd60e8fede
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-21 16:37:48 +0500

    Guid Added in Role

--- RestApiTask/Models/Roles/Payments.cs
+++ RestApiTask/Models/Roles/Payments.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,8 @@ namespace RestApiTask.Models
 {
     public class Payments
     {
-        [Key]
-        public int Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
         [Display(Name = "View Payment")]
         public bool ViewPayment { get; set; }
         [Display(Name = "Change Status")]
