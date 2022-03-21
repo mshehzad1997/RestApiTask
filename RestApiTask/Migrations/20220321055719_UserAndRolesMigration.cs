a52657fa4e102fa7cc36060591865a0b4694de7c
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-21 11:01:18 +0500

    Configuration Added

---
+++ RestApiTask/Migrations/20220321055719_UserAndRolesMigration.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiTask.Migrations
+{
+    public partial class UserAndRolesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_demoRequests_manageRoles_ManageRolesId",
+                table: "demoRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manageTenants_manageRoles_ManageRolesId",
+                table: "manageTenants");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manageUsers_userManagements_UserManagementsId",
+                table: "manageUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_manageRoles_ManageRolesId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_rolesManages_userManagements_UserManagementsId",
+                table: "rolesManages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_userManagements_manageRoles_ManageRolesId",
+                table: "userManagements");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_demoRequests_manageRoles_ManageRolesId",
+                table: "demoRequests",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manageTenants_manageRoles_ManageRolesId",
+                table: "manageTenants",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manageUsers_userManagements_UserManagementsId",
+                table: "manageUsers",
+                column: "UserManagementsId",
+                principalTable: "userManagements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_manageRoles_ManageRolesId",
+                table: "Payments",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_rolesManages_userManagements_UserManagementsId",
+                table: "rolesManages",
+                column: "UserManagementsId",
+                principalTable: "userManagements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_userManagements_manageRoles_ManageRolesId",
+                table: "userManagements",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_demoRequests_manageRoles_ManageRolesId",
+                table: "demoRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manageTenants_manageRoles_ManageRolesId",
+                table: "manageTenants");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manageUsers_userManagements_UserManagementsId",
+                table: "manageUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_manageRoles_ManageRolesId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_rolesManages_userManagements_UserManagementsId",
+                table: "rolesManages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_userManagements_manageRoles_ManageRolesId",
+                table: "userManagements");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_demoRequests_manageRoles_ManageRolesId",
+                table: "demoRequests",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manageTenants_manageRoles_ManageRolesId",
+                table: "manageTenants",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manageUsers_userManagements_UserManagementsId",
+                table: "manageUsers",
+                column: "UserManagementsId",
+                principalTable: "userManagements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_manageRoles_ManageRolesId",
+                table: "Payments",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_rolesManages_userManagements_UserManagementsId",
+                table: "rolesManages",
+                column: "UserManagementsId",
+                principalTable: "userManagements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_userManagements_manageRoles_ManageRolesId",
+                table: "userManagements",
+                column: "ManageRolesId",
+                principalTable: "manageRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- RestApiTask/Models/Data/ApplicationDbContext.cs
+++ RestApiTask/Models/Data/ApplicationDbContext.cs
@@ -24,53 +24,59 @@ namespace RestApiTask.Models.Data
         {
             modelBuilder.Entity<Payments>()
            .HasOne(p => p.ManageRoles)
-           .WithMany(b => b.Payments);
+           .WithMany(b => b.Payments)
+           .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<ManageTenant>()
           .HasOne(p => p.ManageRoles)
-          .WithMany(b => b.ManageTenants);
+          .WithMany(b => b.ManageTenants)
+          .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<DemoRequest>()
           .HasOne(p => p.ManageRoles)
-          .WithMany(b => b.DemoRequests);
+          .WithMany(b => b.DemoRequests)
+          .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<ManageUsers>()
          .HasOne(p => p.UserManagements)
-         .WithMany(b => b.manageUsers);
+         .WithMany(b => b.manageUsers)
+         .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<RolesManage>()
          .HasOne(p => p.UserManagements)
-         .WithMany(b => b.rolesManages);
+         .WithMany(b => b.rolesManages)
+         .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<UserManagements>()
          .HasOne(p => p.ManageRoles)
-         .WithMany(b => b.UserManagements);
+         .WithMany(b => b.UserManagements)
+         .OnDelete(DeleteBehavior.Cascade);
 
       //      modelBuilder.Entity<Payments>()
       //                .HasOne(p => p.ManageRoles)
       //                .WithMany(b => b.Payments)
       //              .OnDelete(DeleteBehavior.Cascade);
 
-      //        modelBuilder.Entity<ManageTenant>()
-      //        .HasOne(p => p.ManageRoles)
-      //       .WithMany(b => b.ManageTenants)
-      //       .OnDelete(DeleteBehavior.Cascade);
+            //        modelBuilder.Entity<ManageTenant>()
+            //        .HasOne(p => p.ManageRoles)
+            //       .WithMany(b => b.ManageTenants)
+            //       .OnDelete(DeleteBehavior.Cascade);
 
-           
-      //      modelBuilder.Entity<DemoRequest>()
-      //    .HasOne(p => p.ManageRoles)
-      //    .WithMany(b => b.DemoRequests)
-      //       .OnDelete(DeleteBehavior.Cascade);
 
-      //          modelBuilder.Entity<ManageUsers>()
-      //    .HasOne(p => p.UserManagements)
-      //    .WithMany(b => b.manageUsers)
-      //      .OnDelete(DeleteBehavior.Cascade);
+            //      modelBuilder.Entity<DemoRequest>()
+            //    .HasOne(p => p.ManageRoles)
+            //    .WithMany(b => b.DemoRequests)
+            //       .OnDelete(DeleteBehavior.Cascade);
 
-      //      modelBuilder.Entity<RolesManage>()
-      // .HasOne(p => p.UserManagements)
-      // .WithMany(b => b.rolesManages)
-      // .OnDelete(DeleteBehavior.Cascade);
+            //          modelBuilder.Entity<ManageUsers>()
+            //    .HasOne(p => p.UserManagements)
+            //    .WithMany(b => b.manageUsers)
+            //      .OnDelete(DeleteBehavior.Cascade);
 
-      //      modelBuilder.Entity<UserManagements>()
-      //.HasOne(p => p.ManageRoles)
-      //.WithMany(b => b.UserManagements)
-      //.OnDelete(DeleteBehavior.Cascade);
+            //      modelBuilder.Entity<RolesManage>()
+            // .HasOne(p => p.UserManagements)
+            // .WithMany(b => b.rolesManages)
+            // .OnDelete(DeleteBehavior.Cascade);
+
+            //      modelBuilder.Entity<UserManagements>()
+            //.HasOne(p => p.ManageRoles)
+            //.WithMany(b => b.UserManagements)
+            //.OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
