66b2b444a20f8b3466ce5cadb27554a7a6b4938b
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-24 18:11:53 +0500

    migration Added

---
+++ RestApiTask/Migrations/20220324130940_RoleIdAdded.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiTask.Migrations
+{
+    public partial class RoleIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManageRoleId",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManageRoleId",
+                table: "UserRoles",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManageRoleId",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "ManageRoleId",
+                table: "UserRoles");
+        }
+    }
+}
