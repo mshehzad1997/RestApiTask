b7fea3bf75d855f5ff7df0e39768fdcc80288d4e
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-24 18:03:02 +0500

    Migration Run

--- RestApiTask/Migrations/20220324123538_manytomanys.Designer.cs
+++ RestApiTask/Migrations/20220324123538_manytomanys.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestApiTask.Models.Data;
 
 namespace RestApiTask.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220324123538_manytomanys")]
+    partial class manytomanys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestApiTask/Migrations/20220324123538_manytomanys.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApiTask.Migrations
+{
+    public partial class manytomanys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManageRoleId",
+                table: "users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManageRoleId",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
