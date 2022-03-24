306a398058f1c016318b293a677c062776ce1371
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-24 17:17:50 +0500

    Relation Added

--- RestApiTask/Models/Data/ApplicationDbContext.cs
+++ RestApiTask/Models/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using RestApiTask.Models.User;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +48,11 @@ namespace RestApiTask.Models.Data
          .HasOne(p => p.ManageRoles)
          .WithMany(b => b.UserManagements)
          .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<UserRoles>()
+                .HasKey(ur => new { ur.RoleId, ur.UserId });
+            modelBuilder.Entity<UserRoles>()
+                .HasOne(ur => ur.Users)
+                .WithMany(ur => ur.ManagRoles)
        
       //      modelBuilder.Entity<Payments>()
       //                .HasOne(p => p.ManageRoles)
